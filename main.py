1f3a413a8ef51aae18819517d6f22f72e15d2364
mrmarouanjamal-web <mr.marouanjamal@gmail.com>
2026-05-25 11:22:21 +0100

    Added task saving feature with tasks.txt

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-tasks = []
+# Load tasks from file
+try:
+    with open("tasks.txt", "r") as file:
+        tasks = file.read().splitlines()
+except:
+    tasks = []
+
 
 while True:
     print("\n=== TASK MANAGER ===")
@@ -9,22 +15,37 @@ while True:
 
     choice = input("Choose: ")
 
+    # Add task
     if choice == "1":
         task = input("Enter task: ")
         tasks.append(task)
+
+        with open("tasks.txt", "w") as file:
+            for t in tasks:
+                file.write(t + "\n")
+
         print("Task added!")
 
+    # Show tasks
     elif choice == "2":
         print("\nTasks:")
         for i, task in enumerate(tasks):
             print(f"{i+1}. {task}")
 
+    # Delete task
     elif choice == "3":
         num = int(input("Task number to delete: "))
+
         if 0 < num <= len(tasks):
-            removed = tasks.pop(num-1)
+            removed = tasks.pop(num - 1)
+
+            with open("tasks.txt", "w") as file:
+                for t in tasks:
+                    file.write(t + "\n")
+
             print(f"{removed} deleted")
 
+    # Exit
     elif choice == "4":
         print("Bye!")
         break
